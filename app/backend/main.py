ffc641cffafa31f0299fd2837de4edada5e8edd0
JbIPS <jeanbaptiste.richardet@gmail.com>
2022-02-28 20:35:41 +0100

    Création du backend
    * Route de l'API

---
+++ app/backend/main.py
@@ -0,0 +1,25 @@
+import uvicorn
+import joblib
+from fastapi import FastAPI
+
+app = FastAPI()
+
+model = joblib.load('pipeline.joblib')
+mlb = joblib.load('mlb.joblib')
+
+
+@app.get("/")
+def home():
+    return {"Hello": "World"}
+
+
+@app.get("/prediction")
+def get_prediction(data: str):
+    print(data)
+    print(type(data))
+    predicted_tags = model.predict([data])
+    return {'Tags': mlb.inverse_transform(predicted_tags)}
+
+
+if __name__ == "__main__":
+    uvicorn.run("fastapi:app")
